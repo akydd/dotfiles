97bd2f4ab0fd8352edf21e84069a69a11d544fd1
Alan Kydd <akydd@ualberta.net>
2022-10-13 22:36:40 -0600

    Tried to fix autoformat for md

--- neovim/after/ftplugin/markdown.lua
+++ neovim/after/ftplugin/markdown.lua
@@ -1,2 +1,5 @@
 vim.cmd [[ set textwidth=80 ]]
 vim.cmd [[ set formatoptions+=ta ]]
+-- TODO: fix this for better comment autoformatting in markdown!
+vim.cmd [[ set formatoptions-=c ]]
+vim.cmd [[ set formatoptions-=j ]]
